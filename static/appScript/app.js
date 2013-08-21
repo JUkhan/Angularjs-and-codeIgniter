4b65d4a43dce8d0e3a0dd6ae218c635ccec6880a
jasim-uddin-khan <jasim.uddin.khan@gmail.com>
2013-08-21 15:46:48 +0600

    add user guide

--- static/appScript/app.js
+++ static/appScript/app.js
@@ -2,10 +2,11 @@ angular.module('project', ['ui.bootstrap', 'ngGrid']).
   config(function($routeProvider) {
     $routeProvider.
       when('/', { templateUrl:BASE_URL+'home_ctrl'}).
-      		
+      		when('/Department', { templateUrl:BASE_URL+'Department_ctrl'}).
 		when('/Navigations', { templateUrl:BASE_URL+'Navigations_ctrl'}).
-		when('/NavigViewRight', { templateUrl:BASE_URL+'NavigViewRight_ctrl'}).		
-		when('/Roles', { templateUrl:BASE_URL+'Roles_ctrl'}).		
+		when('/NavigViewRight', { templateUrl:BASE_URL+'NavigViewRight_ctrl'}).
+		when('/Roles', { templateUrl:BASE_URL+'Roles_ctrl'}).
+		when('/Student', { templateUrl:BASE_URL+'Student_ctrl'}).
 		when('/Users', { templateUrl:BASE_URL+'Users_ctrl'}).
 
       otherwise({redirectTo:'/'});
