2fa42afc3a1da72bc6764074bb7af36c745d4ee5
jasim-uddin-khan <jasim.uddin.khan@gmail.com>
2013-08-23 22:31:43 +0600

    login page update

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,75 @@
-<form action="<?php echo site_url();?>/login/submit" method="post">
-  <p>Username: <input type="text" name="username" value="" />
-  <p>Password: <input type="password" name="password" value="" />
-  <p><input type="submit" name="submit" value="Login"  />
-</form>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Sign in &middot; </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+ <link href="../static/css/bootstrap.css" rel="stylesheet">
+ 
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+
+      .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+    </style>
+  
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+   
+  </head>
+
+  <body>
+
+    <div class="container">
+      <form class="form-signin" action="<?php echo site_url();?>/login/submit" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="input-block-level" name="username" value="" placeholder="Email address">
+        <input type="password" class="input-block-level" name="password" value="" placeholder="Password">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->  
+<script src="../static/js/jquery-1.9.1.js"></script>
+   
+ <script src="../static/js/bootstrap.min.js"></script>	
+    
+  </body>
+</html>
